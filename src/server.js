5d1c6e77a66b2cfa4d9ce2ab7d86b1673179fa9d
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-03 18:06:57 +0200

    dir path

--- src/server.js
+++ src/server.js
@@ -10,9 +10,9 @@ const forceSSL = function() {
     next();
   };
 };
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/kanaliiga-stats/index.html'));
-});
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist/kanaliiga-stats'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + 'dist', 'index.html'));
+});
+app.use(express.static(__dirname + 'dist'));
 app.listen(process.env.PORT || 8080);
