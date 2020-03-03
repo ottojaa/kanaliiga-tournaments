5828b5512912e1564db3171f2359914920f483f5
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-03 17:50:13 +0200

    filepath

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const forceSSL = function() {
   };
 };
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/kanaliiga-stats/index.html'));
 });
 app.use(forceSSL());
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/kanaliiga-stats'));
 app.listen(process.env.PORT || 8080);
