4e62470a7e9c954c81cf2b4a718e4799dd97b9f4
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-03 18:12:03 +0200

    directory fix

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const forceSSL = function() {
   };
 };
 app.use(forceSSL());
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + 'dist', 'index.html'));
 });
-app.use(express.static(__dirname + 'dist'));
 app.listen(process.env.PORT || 8080);
