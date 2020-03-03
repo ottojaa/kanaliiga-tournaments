124baeae467add3d69a53a73e8894bf4dbfc031f
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-03 18:38:38 +0200

    revert dist

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const forceSSL = function() {
   };
 };
 app.use(forceSSL());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, '/dist')));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 app.listen(process.env.PORT || 8080);
