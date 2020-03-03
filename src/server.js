6bf3b95605103849956d53f57cc89a40ae260de7
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-03 17:17:20 +0200

    express

--- src/server.js
+++ src/server.js
@@ -1,12 +1,4 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-app.use(express.static(__dirname + '/dist/kanaliiga-tournaments'));
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/kanaliiga-tournaments/index.html'));
-});
-
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
