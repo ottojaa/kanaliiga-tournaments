7c34fdc46624ac758fdaf86dbad13ba88d3bc7d4
otto.jaakonmaki <otto.jaakonmaki@cgi.com>
2020-03-30 16:01:23 +0300

    leaderboards

---
+++ src/app/components/teams-list/teams-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { MatTableDataSource } from '@angular/material';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-teams-list',
+  templateUrl: './teams-list.component.html',
+  styleUrls: ['./teams-list.component.scss'],
+})
+export class TeamsListComponent implements OnInit {
+  @Input() teams: Observable<any>;
+  displayedColumns: string[] = [
+    'index',
+    'name',
+    'played',
+    'wins',
+    'losses',
+    'scoreFor',
+    'scoreAgainst',
+    'forfeits',
+    'plusMinus',
+    'points',
+  ];
+
+  tooltips = [
+    { column: 'played', tooltip: 'Played' },
+    { column: 'wins', tooltip: 'Wins' },
+    { column: 'losses', tooltip: 'Losses' },
+    { column: 'scoreFor', tooltip: 'Score for' },
+    { column: 'scoreAgainst', tooltip: 'Score against' },
+    { column: 'forfeits', tooltip: 'Forfeits' },
+    { column: 'plusMinus', tooltip: 'Total score' },
+  ];
+  dataSource: MatTableDataSource<any>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.teams.subscribe(data => {
+      this.dataSource = new MatTableDataSource(data.data);
+      this.dataSource.data = data.data;
+    });
+  }
+
+  getTooltip(name: string): string {
+    return this.tooltips.find(tooltip => tooltip.column === name).tooltip;
+  }
+}
